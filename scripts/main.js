814a8e5b4609b104a125ef2ff9dd49bf2958caf2
Edwxn <edwinpena0306@gmail.com>
2024-12-13 15:34:41 -0400

    arreglo: style.css creación: main.js

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById('generateBtn');
+const randomNumberEl = document.getElementById('numberGenerated');
+
+function generateRandomNumber() {
+    let minNum = parseInt(document.getElementById('minNum').value);
+    let maxNum = parseInt(document.getElementById('maxNum').value);
+
+    if(minNum !== '' && maxNum !== '' && minNum < maxNum) {
+        randomNumberEl.style.display = 'flex';
+
+        let randomNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        randomNumberEl.innerText = randomNumber;
+    }
+
+    else {
+        randomNumberEl.style.display = 'flex';
+        randomNumberEl.innerText = 'Error';
+
+        setTimeout(function() {
+            randomNumberEl.innerText = '0';
+            randomNumberEl.style.display = 'none';
+        }, 1600);
+    }
+}
+
+btn.addEventListener('click', generateRandomNumber);
